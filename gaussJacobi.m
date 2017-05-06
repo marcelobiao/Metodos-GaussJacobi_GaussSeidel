afa58f862fc497d9bd15282ee1f57b0eda672af1
marcelobiao <marcelobiao2@gmail.com>
2017-05-06 09:48:44 -0300

    Refatorando

--- gaussJacobi.m
+++ gaussJacobi.m
@@ -1,17 +1,17 @@
 function raizes = gaussJacobi(Ax,b,x0,e,maxi);
-    fprintf('Método de GaussJacobi:\n');
     [m,n]=size(Ax);
+    raizes=zeros(m,maxi);
+    x=zeros(m,1);
     
     for k=1:maxi
         for i=1:n
-            Sum1 = b(i)-Ax(i,[1:i-1,i+1:n])*x0([1:i-1,i+1:n]);
-            x(i,1) = Sum1/Ax(i,i);
-        end
+            x(i,1) = (b(i)-Ax(i,[1:i-1,i+1:n])*x0([1:i-1,i+1:n]))/Ax(i,i);
+        end        
         erro=abs(norm(x-x0));
         x0=x;
+        raizes(:,k)=x;
         if (erro<e)
             break;
         end        
     end
-    raizes=x;
 end
--- main.m
+++ main.m
@@ -16,7 +16,18 @@ x0=[0;
 
 %Precisao e limite de execucoes
 e=0.0001;
-maxi=20;
+maxi=30;
 
 raizesJacobi = gaussJacobi(Ax,b,x0,e,maxi);
-disp(raizesJacobi);
+%disp(raizesJacobi');
+raizesSeidel = gaussSeidel(Ax,b,x0,e,maxi);
+%disp(raizesSeidel');
+
+%Saida dos metodos
+fprintf('\t\t\tgaussJacobi \t\t- \t\tgaussSeidel\n');
+for i=1:maxi
+    if(norm(raizesJacobi(:,i))==0 && norm(raizesSeidel(:,i))==0)
+        break;
+    end
+    fprintf('k=%d \t%.4f \t%.4f \t%.4f \t-\t%.4f \t%.4f \t%.4f\n',i,raizesJacobi(:,i)',raizesSeidel(:,i)');
+end
